4debd7609052f5ff349bdfb0cabeb34908cf6796
angelinalu <alu@elon.edu>
2019-07-16 22:23:42 -0400

    animation tests

---
+++ fencing simulator/Assets/scripts/Scoring.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scoring : MonoBehaviour
+{
+    private int opponent;
+    private int player;
+    private int point;
+    private int rightOfWay;
+    private int attacking;
+    private int missed;
+    private int parried;
+    private int hit;
+
+    private bool ongoing;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        point = 0;
+        ongoing = true;
+        rightOfWay = 0;
+        attacking = 0;
+        missed = 0;
+        parried = 0;
+        hit = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        while (ongoing)
+        {
+            if (point == -1 && opponent < 15)
+            {
+                opponent++;
+            }
+            else if (point == 1 && player < 15)
+            {
+                player++;
+            }
+            ongoing &= opponent != 15 && player != 15;
+        }
+    }
+}
