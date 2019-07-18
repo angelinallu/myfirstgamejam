5539b3c00645a5ae818f0f079d75948feafc2e8e
angelinalu <alu@elon.edu>
2019-07-18 17:27:31 -0400

    functional settings menu

---
+++ fencing simulator/Assets/scripts/VolumeSlider.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class VolumeSlider : MonoBehaviour
+{
+
+    public AudioMixer mixer;
+    public Slider slider;
+
+    void Start()
+    {
+        if (PlayerPrefs.HasKey("MusicVolume"))
+        {
+            slider.value = PlayerPrefs.GetFloat("MusicVolume", 0.75f);
+        }
+        else
+        {
+            PlayerPrefs.SetFloat("MusicVolume", slider.value);
+        }
+    }
+    public void SetLevel(float sliderValue)
+    {
+        mixer.SetFloat("Volume", Mathf.Log10(sliderValue) * 20);
+        PlayerPrefs.SetFloat("MusicVolume", sliderValue);
+    }
+}
